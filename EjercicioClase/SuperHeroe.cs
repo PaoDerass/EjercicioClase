c4ea54d33c1e157c638956012f046c9758da1ebe
PaoDerass <paola_deras@unitec.edu>
2024-07-26 13:59:58 -0600

    creacion de clases

---
+++ EjercicioClase/SuperHeroe.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace EjercicioClase
+{
+    public class SuperHeroe
+    {
+        public string nombre { get; set; }
+        public string identidadSecreta { get; set; }
+        public string ciudad { get; set; }
+
+        public bool puedeVolar { get; set; }
+
+        public string superPoder SuperPoder { get; set; }
+
+    }
+}
---
+++ EjercicioClase/SuperPoder.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace EjercicioClase
+{
+    public class SuperPoder
+    {
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public string nivel { get; set; }
+    }
+}
